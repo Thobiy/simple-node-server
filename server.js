016191a03f2ed888f05711c9bc140e53ed2ce994
Thobiy <tobiemmanuel19@gmail.com>
2024-05-15 17:27:02 +0100

    Adjusted server file

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ const http = require("node:http");
 
 
 const server = http.createServer((req, res) => {
-    console.log({
-        url: req.url,
-        method: req.method
-});
+    if (req.url === "/" && req.method === "GET") {
+        res.end(JSON.stringify({ message: "API Running" }));
 
-});
+    } else if (req.url === "/jokes" && req.method === "GET") {
+        res.end(JSON.stringify([]));
+
+    } else if (req.url === "/jokes" && req.method === "PUT") {
+        res.end(JSON.stringify( {message: "putting successful" }));
+
+    } else {
+        res.writeHead(404);
+        res.end(JSON.stringify({ message: "Not found" }));
+    }
+}
+
+);
 
 
 
-server.listen(3000, "localhost", () => {});
+server.listen(3000, "127.0.0.1", () => {});
 console.log("server running");
 
 
