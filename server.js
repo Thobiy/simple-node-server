822c96c86c2991a2e7d3ce10f30d1b71bae2c00d
Thobiy <tobiemmanuel19@gmail.com>
2024-05-16 16:53:53 +0100

    updated the server.js

--- server.js
+++ server.js
@@ -1,16 +1,53 @@
 const http = require("node:http");
+const os = require('os');
+
+
+function sysDetail(){
+    const cpuInfo = os.cpus();
+    const osInfo = {
+       release: os.release(),
+       machine: os.machine(),
+       type: os.type(),
+       userInfo: os.userInfo(),
+       memory: os.totalmem(),
+       freeMemory: os.freemem(),
+
+   };
+
+   const details = {
+       cpuInfo: cpuInfo,
+       osInfo: osInfo
+   };
+   return details;
+};
 
 
 const server = http.createServer((req, res) => {
+    res.setHeader('Access-control-Allow-Origin', "*");
+    res.setHeader("Access-control-Allow-methods", "GET");
+    res.setHeader("Access-control-Allow-Headers", "Content-Type");
+
+    
+
+
     if (req.url === "/" && req.method === "GET") {
-        res.end(JSON.stringify({ message: "API Running" }));
+       // res.end(JSON.stringify({ message: "API Running" }));
+
+        const systemDetail = sysDetail();
+        res.writeHead(200,  {"content-Type": 'text/json' });
+        res.end(JSON.stringify(systemDetail));
+
+
+
 
     } else if (req.url === "/jokes" && req.method === "GET") {
         res.end(JSON.stringify([]));
 
     } else if (req.url === "/jokes" && req.method === "PUT") {
-        res.end(JSON.stringify( {message: "putting successful" }));
+        res.end(JSON.stringify({message: "putting successful" }));
 
+    } else if (req.url === "/" && req.method === "POST") {
+        res.end(JSON.stringify({message: "Done"}));
     } else {
         res.writeHead(404);
         res.end(JSON.stringify({ message: "Not found" }));
